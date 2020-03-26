445fafb6c4040937b3ce51f085256efcac51f536
GitHub-Sebastien <59145944+GitHub-Sebastien@users.noreply.github.com>
2020-03-26 19:06:08 +0100

    Let's rock

--- public/static/js/objects.js
+++ public/static/js/objects.js
@@ -1,29 +1,37 @@
-/**
- * Fonction à appeler au chargement de la page
- * Cette fonction devra exécuter les actions suivantes :
- *    - charger la liste des objets depuis l'API
- *    - charger les données des objets dans la table
- */
+var object_data = {
+    "serial": "OBJ_001",
+    "type": "raspberry_TH",
+    "image": "raspberry-pi-4.jpg",
+    "description": "Capteur de température et d'humidité de la salle de cours du Campus de Chambéry",
+    "location": "45.644065, 5.867810",
+    "refresh": 5,
+    "status": true,
+    "provisionning": {
+        "date": "2020-03-20",
+        "operator": "JPA"
+    }
+};
+
 function load_components() {
     console.log("Chargement des données de la page");
-    $.get("/objects", function(data, status) {
-        console.log(data);
+
+    $.get("/objects", function(data) {
+        for (let d in data.objects) {
+            add_line_to_table(data.objects[d]);
+        }
     });
     // Ajouter ici le code permettant de charger dynamiquement les éléments de la page
 }
 
 function add_line_to_table(data) {
+
     let line = '<tr> \
-    <th style = "width: 100px" > N° Série:' + data.serial + '  </th> \
-    <th > Image </th>  \
-    <th > Description </th> \
+    <th style = "width: 100px" >' + data.serial + '  </th> \
+    <th ><img src="<img src="./static/images/raspberry-pi-4.jpg" width="300" heigth="300"></th>  \
+    <th > Description ' + data.description + ' </th> \
     <th style = "width: 100px"><input type="checkbox"></th> \
     <th style = "width: 100px"><button class="btn-primary">Submit !</button></th> \
     </tr> ';
     //$('#table_body').append(line);
-
-    // var node = document.createElement("tr");
-    // var textnode = document.create(line);
-    // node.appendChild(textnode);
     document.getElementById('table_body').innerHTML += line;
 }
