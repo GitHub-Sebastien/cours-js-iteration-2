e268b093e2d23fa6babe409eb89e96eabadb00e5
GitHub-Sebastien <59145944+GitHub-Sebastien@users.noreply.github.com>
2020-03-26 17:31:10 +0100

    Maj

--- public/static/js/objects.js
+++ public/static/js/objects.js
@@ -6,7 +6,24 @@
  */
 function load_components() {
     console.log("Chargement des données de la page");
+    $.get("/objects", function(data, status) {
+        console.log(data);
+    });
     // Ajouter ici le code permettant de charger dynamiquement les éléments de la page
 }
 
-load_components();
+function add_line_to_table(data) {
+    let line = '<tr> \
+    <th style = "width: 100px" > N° Série:' + data.serial + '  </th> \
+    <th > Image </th>  \
+    <th > Description </th> \
+    <th style = "width: 100px"><input type="checkbox"></th> \
+    <th style = "width: 100px"><button class="btn-primary">Submit !</button></th> \
+    </tr> ';
+    //$('#table_body').append(line);
+
+    // var node = document.createElement("tr");
+    // var textnode = document.create(line);
+    // node.appendChild(textnode);
+    document.getElementById('table_body').innerHTML += line;
+}
